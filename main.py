4fa79500d25abe6f5193960f42be9cb9efd70b04
Dexter◥▶_◀◤ <335ishwar@gmail.com>
2026-09-01 20:52:33 +0530

    Side bar componet is done

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from services.auth.login_wall import render_login_wall
+from services.state.session_defaults import initial_session_defaults
+from services.components.style_side_bar import style_side_bar
+from services.components.side_bar import render_sidebar
 
 def main():
     st.set_page_config(page_icon=r'static\icons\dumbbell.png',page_title='AI Gym Trainer',
@@ -7,6 +10,10 @@ def main():
                        layout='centered')
     if not render_login_wall():
         return
-    st.write('welcome')
+
+    initial_session_defaults()
+
+    style_side_bar()
+    render_sidebar()
 if __name__ =="__main__":
     main()
